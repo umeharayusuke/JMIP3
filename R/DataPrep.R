91d236c8a96a587d866d9abc6a6da9b6fa01a402
umeharayusuke <umehara.yusuke.26u@st.kyoto-u.ac.jp>
2026-05-20 16:47:39 +0900

    Fix DataPrep code for JMIP submission. Now just need to run the code here and JPN_IAMC.xlsx file will be generated in the same directory, and just submit it.

--- R/DataPrep.R
+++ R/DataPrep.R
@@ -11,6 +11,8 @@ library(purrr)
 library(rnaturalearthdata)
 library(rnaturalearth)
 library(readxl)
+library(writexl)
+library(openxlsx)
 
 theme_1 <- theme_bw() +
   theme(text = element_text(size = 16),
@@ -19,25 +21,28 @@ theme_1 <- theme_bw() +
         legend.position = "right", 
         #legend.title = element_blank(),
         strip.background = element_blank())
-setwd("data3")
+setwd("data")
+setwd("0520")
+
 
 df0 <- read_csv("JPN_IAMC.csv")
 
-df<- df0 %>% 
+df <- df0 %>% 
   filter(!SCENARIO %in% c("SSP2_BaU_NoCC_No", "SSP2_CM_NoCC_No", "SSP2i_CM_NoCC_No")) %>% 
-  select(-`2055`,-`2065`, -`2075`, -`2085`, -`2095`)%>%
+  select(-`2055`, -`2065`, -`2075`, -`2085`, -`2095`) %>%
+  
   mutate(SCENARIO = recode(
     SCENARIO,
-    "SSP2i_BaU_NoCC_No"      = "JMIP_3_1__BASELINE",
-    "SSP2i_CM1_NoCC_No"   = "JMIP_3_1__0by2050_20by2100",
-    "SSP2i_CM2_NoCC_No"   = "JMIP_3_1__0by2050_0by2100",
-    "SSP2i_CM3_NoCC_No"   = "JMIP_3_1__0by2050_-5by2100",
-    "SSP2i_CM4_NoCC_No"   = "JMIP_3_1__0by2050_-20by2100",
-    "SSP2i_CM5_NoCC_No"   = "JMIP_3_1__0by2050_-50by2100",
-    "SSP2i_CM6_NoCC_No"   = "JMIP_3_1__0by2050_-100by2100",
-    "SSP2i_CM7_NoCC_No"   = "JMIP_3_1__20by2050_20by2100",
-    "SSP2i_CM8_NoCC_No"   = "JMIP_3_1__20by2050_0by2100",
-    "SSP2i_CM9_NoCC_No"   = "JMIP_3_1__20by2050_-5by2100",
+    "SSP2i_BaU_NoCC_No"    = "JMIP_3_1__BASELINE",
+    "SSP2i_CM1_NoCC_No"    = "JMIP_3_1__0by2050_20by2100",
+    "SSP2i_CM2_NoCC_No"    = "JMIP_3_1__0by2050_0by2100",
+    "SSP2i_CM3_NoCC_No"    = "JMIP_3_1__0by2050_-5by2100",
+    "SSP2i_CM4_NoCC_No"    = "JMIP_3_1__0by2050_-20by2100",
+    "SSP2i_CM5_NoCC_No"    = "JMIP_3_1__0by2050_-50by2100",
+    "SSP2i_CM6_NoCC_No"    = "JMIP_3_1__0by2050_-100by2100",
+    "SSP2i_CM7_NoCC_No"    = "JMIP_3_1__20by2050_20by2100",
+    "SSP2i_CM8_NoCC_No"    = "JMIP_3_1__20by2050_0by2100",
+    "SSP2i_CM9_NoCC_No"    = "JMIP_3_1__20by2050_-5by2100",
     "SSP2i_CM10_NoCC_No"   = "JMIP_3_1__20by2050_-20by2100",
     "SSP2i_CM11_NoCC_No"   = "JMIP_3_1__20by2050_-50by2100",
     "SSP2i_CM12_NoCC_No"   = "JMIP_3_1__20by2050_-100by2100",
@@ -52,13 +57,38 @@ df<- df0 %>%
     "SSP2i_CM21_NoCC_No"   = "JMIP_3_1__-20by2050_-5by2100",
     "SSP2i_CM22_NoCC_No"   = "JMIP_3_1__-20by2050_-20by2100",
     "SSP2i_CM23_NoCC_No"   = "JMIP_3_1__-20by2050_-50by2100",
-    "SSP2i_CM24_NoCC_No"   = "JMIP_3_1__-20by2050_-100by2100",
-  ))%>% 
+    "SSP2i_CM24_NoCC_No"   = "JMIP_3_1__-20by2050_-100by2100"
+  )) %>%
+  
   mutate(MODEL = "AIM/Hub-Japan 2.4") %>% 
-  mutate(REGION = "Japan")
+  mutate(REGION = "Japan") %>%
+  
+  mutate(VARIABLE = recode(
+    VARIABLE,
+    "Trade|Emissions Allowances [Volume]" = "Trade|Emission Allowance"
+  )) %>%
+  
+  mutate(
+    UNIT = recode(
+      UNIT,
+      "USD_2010/kW"            = "US$2010/kW",
+      "USD_2010/kW/yr"            = "US$2010/kW/yr",
+      "billion USD_2010/yr"    = "billion US$2010/yr",
+      "billion tkm/yr"         = "bn tkm/yr",
+      "billion pkm/yr"         = "bn pkm/yr",
+      "billion USD_2017/yr"    = "billion US$2010/yr",
+      "USD_2010/t CO2"         = "US$2010/t CO2",
+      "USD_2010/GJ"            = "US$2010/GJ"
+    ),
+    UNIT = if_else(
+      VARIABLE == "Trade|Emission Allowance" &
+        UNIT == "Mt CO2-equiv/yr",
+      "Mt CO2/yr",
+      UNIT
+    )
+  )
 
-write.csv(df, "JPN_IAMC.csv", row.names = FALSE)
-
+write.xlsx(df, "JPN_IAMC.xlsx", rowNames = FALSE)
 
 df_ref <- read_excel(
   "Updated JMIP data template 20251219.xlsx",
